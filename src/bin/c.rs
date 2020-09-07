eed63fd5b4786be484b102deeaca970deedccebb
masa <masatoshi.sboard@gmail.com>
2020-09-07 15:38:39 +0900

    Done c

--- src/bin/c.rs
+++ src/bin/c.rs
@@ -4,5 +4,24 @@ use proconio::{fastout, input};
 #[fastout]
 fn main() {
     input! {
+        n: usize,
+        s: Chars,
     }
+
+    let mut cur = 0;
+    for i in s.iter() {
+        if *i == 'E' {
+            cur += 1;
+        }
+    }
+    let mut ans = cur;
+    for i in 0..n {
+        if s[i] == 'E' {
+            cur -= 1;
+        } else {
+            cur += 1;
+        }
+        ans = std::cmp::min(ans, cur);
+    }
+    println!("{}", ans);
 }
